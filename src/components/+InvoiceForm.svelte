93c981c99b8bf4d17d5dc295e0cebabb614a42c9
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-03-04 20:18:33 -0300

    Chore: structuring information

--- src/components/+InvoiceForm.svelte
+++ src/components/+InvoiceForm.svelte
@@ -2,41 +2,51 @@
     
 </script>
 
-<div class="bg-green-400 container mx-auto p-4 m-4">
+<div class="bg-green-400 container mx-auto p-4 m-4 w-[45vw]">
     <div class="w-full bg-white rounded-lg shadow-md overflow-hidden">
-        <header class="flex justify-between border" >
-            <div class="bg-red-200 p-4 border-b">
-                <h1 class="text-2xl font-normal">Bernardo Camilo Ferrari</h1>
-            </div>
+      <header class="flex justify-between mb-4" >
+        <div class="bg-red-200 p-4 border-b">
+          <h1 class="text-2xl font-normal mb-2">Bernardo Camilo Ferrari</h1>
 
-            <div class="flex flex-col w-1/2 p-2">
-              <div class= "flex">
-                  <h1 class="text-2xl font-semibold mr-2">Invoice:</h1>
-                  <input class="text-gray-500 w-1/2" placeholder="Invoice #INV1234" type="text" />
-              </div>
+          <div>
+            <h2 class="text-lg font-semibold">Contact details:</h2>
 
-              <div>
-                <div>
-                  <p>Issue date: </p>
-                  <input type="date" />
-                </div>
-                <div>
-                  <p>Due by: </p>
-                  <input type="date" />
-                </div>
+            <div>
+              <p>Mariano Moreno 960, Ing. Maschwitz</p>
+              <p>1623 Buenos Aires, Argentina</p>
+              <p>bdki.development@gmail.com</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex flex-col bg-blue-200 p-2">
+          <div class= "flex justify-end mb-2">
+            <h1 class="text-2xl font-semibold mr-2">Invoice:</h1>
+            <input class="text-gray-500 w-1/2" placeholder="Invoice #INV1234" type="text" />
+          </div>
+          
+          <div class= "flex flex-col">
+            <h2 class="text-lg font-semibold">Invoice Information</h2>
+
+            <div class="w-full flex flex-col items-end">
+              <div class="flex">
+                <p>Issue date: </p>
+                <input type="date" />
+              </div>
+              <div class="flex">
+                <p>Due by: </p>
+                <input type="date" />
               </div>
             </div>
-        </header>
+          </div>
+        </div>
+      </header>
         
       <div class="p-4">
         <div class="flex justify-between mb-4">
           <div>
             <h2 class="text-lg font-semibold">Client Information</h2>
             <p class="text-gray-500">John Doe<br>Email: john@example.com<br>Phone: +123456789</p>
-          </div>
-          <div>
-            <h2 class="text-lg font-semibold">Invoice Information</h2>
-            <p class="text-gray-500">Date: March 4, 2024<br>Due: March 11, 2024</p>
           </div>
         </div>
         <table class="w-full mb-8">
