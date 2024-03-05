8ac16f4d8ca5e6852fa1086442c7c374e442e1f5
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-03-05 14:47:18 -0300

    Chore: adding input fields + adjusting table width

--- src/components/+InvoiceForm.svelte
+++ src/components/+InvoiceForm.svelte
@@ -21,21 +21,33 @@
 
         <div class="flex flex-col bg-blue-200 p-2">
           <div class= "flex justify-end mb-2">
-            <h1 class="text-2xl font-semibold mr-2">Invoice:</h1>
-            <input class="text-gray-500 w-1/2" placeholder="Invoice #INV1234" type="text" />
-          </div>
-          
-          <div class= "flex flex-col">
-            <h2 class="text-lg font-semibold">Invoice Information</h2>
-
-            <div class="w-full flex flex-col items-end">
-              <div class="flex">
-                <p>Issue date: </p>
-                <input type="date" />
-              </div>
-              <div class="flex">
-                <p>Due by: </p>
-                <input type="date" />
+            <div>
+              <h2 class="text-lg font-semibold">Client details</h2>
+              <div class="flex flex-col">
+                <div>
+                  <label class="flex flex-col">
+                    Company name:
+                    <input type="text" class="">
+                  </label>
+                </div>
+                <div>
+                  <label class="flex flex-col">
+                    Street adress:
+                    <input type="text" class="">
+                  </label>
+                </div>
+                <div>
+                  <label class="flex flex-col">
+                    Provice & country:
+                    <input type="text" class="">
+                  </label>
+                </div>
+                <div>
+                  <label class="flex flex-col">
+                    Representative email:
+                    <input type="text" class="">
+                  </label>
+                </div>
               </div>
             </div>
           </div>
@@ -44,16 +56,28 @@
         
       <div class="p-4">
         <div class="flex justify-between mb-4">
-          <div>
-            <h2 class="text-lg font-semibold">Client Information</h2>
-            <p class="text-gray-500">John Doe<br>Email: john@example.com<br>Phone: +123456789</p>
+          <div class= "flex flex-col">
+            <div class="w-full flex flex-col items-end">
+              <div class="flex">
+                <p class="mr-2">Issue date: </p>
+                <input type="date" />
+              </div>
+              <div class="flex">
+                <p >Due by: </p>
+                <input type="date" />
+              </div>
+            </div>
+          </div>
+
+          <div class="flex items-center">
+            <h1 class="text-xl font-semibold mr-2">Invoice:</h1>
+            <input class="w-1/2 text-gray-500 bg-gray-200" placeholder="Invoice #INV1234" type="text" />
           </div>
         </div>
         <table class="w-full mb-8">
           <thead>
             <tr>
-              <th class="border px-4 py-2">#</th>
-              <th class="border px-4 py-2">Description</th>
+              <th class="flex justify-start border px-4 py-2">Product/Service</th>
               <th class="border px-4 py-2">Quantity</th>
               <th class="border px-4 py-2">Unit Price</th>
               <th class="border px-4 py-2">Total</th>
@@ -61,8 +85,7 @@
           </thead>
           <tbody>
             <tr>
-              <td class="border px-4 py-2">1</td>
-              <td class="border px-4 py-2">Service A</td>
+              <td class="border px-4 py-2">Front End Development</td>
               <td class="border px-4 py-2">2</td>
               <td class="border px-4 py-2">$50.00</td>
               <td class="border px-4 py-2">$100.00</td>
