aed55ccb839305b6e8670537e99ccf485e497fc9
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-03-05 15:23:58 -0300

    Styling: adding some spacing

--- src/components/+InvoiceForm.svelte
+++ src/components/+InvoiceForm.svelte
@@ -2,10 +2,10 @@
     
 </script>
 
-<div class="bg-green-400 container mx-auto p-4 m-4 w-[45vw]">
-    <div class="w-full bg-white rounded-lg shadow-md overflow-hidden">
+<div class="container mx-auto m-4 w-[45vw]">
+    <div class="w-full p-4 bg-white rounded-lg shadow-md overflow-hidden">
       <header class="flex justify-between mb-4" >
-        <div class="bg-red-200 p-4 border-b">
+        <div class="p-4">
           <h1 class="text-2xl font-normal mb-2">Bernardo Camilo Ferrari</h1>
 
           <div>
@@ -19,7 +19,7 @@
           </div>
         </div>
 
-        <div class="flex flex-col bg-blue-200 p-2">
+        <div class="flex flex-col p-2">
           <div class= "flex justify-end mb-2">
             <div>
               <h2 class="text-lg font-semibold">Client details</h2>
@@ -54,8 +54,8 @@
         </div>
       </header>
         
-      <div class="p-4">
-        <div class="flex justify-between mb-4">
+      <div>
+        <div class="flex justify-between px-4 mb-8">
           <div class= "flex flex-col">
             <div class="w-full flex flex-col items-end">
               <div class="flex">
@@ -74,31 +74,35 @@
             <input class="w-1/2 text-gray-500 bg-gray-200" placeholder="Invoice #INV1234" type="text" />
           </div>
         </div>
-        <table class="w-full mb-8">
-          <thead>
-            <tr>
-              <th class="flex justify-start border px-4 py-2">Product/Service</th>
-              <th class="border px-4 py-2">Quantity</th>
-              <th class="border px-4 py-2">Unit Price</th>
-              <th class="border px-4 py-2">Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td class="border px-4 py-2">Front End Development</td>
-              <td class="border px-4 py-2">2</td>
-              <td class="border px-4 py-2">$50.00</td>
-              <td class="border px-4 py-2">$100.00</td>
-            </tr>
-            <!-- Add more rows for additional services -->
-          </tbody>
-        </table>
-        <div class="flex justify-end">
+
+        <section>
+          <table class="w-full mb-8">
+            <thead>
+              <tr>
+                <th class="flex justify-start border px-4 py-2">Product/Service</th>
+                <th class="border px-4 py-2">Quantity</th>
+                <th class="border px-4 py-2">Unit Price</th>
+                <th class="border px-4 py-2">Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="border px-4 py-2">Front End Development</td>
+                <td class="border px-4 py-2">2</td>
+                <td class="border px-4 py-2">$50.00</td>
+                <td class="border px-4 py-2">$100.00</td>
+              </tr>
+              <!-- Add more rows for additional services -->
+            </tbody>
+          </table>
+        </section>
+
+        <footer class="flex justify-end">
           <div class="text-right">
             <p class="text-lg font-semibold mb-2">Total: $100.00</p>
             <p class="text-gray-500">Amount Due: $100.00</p>
           </div>
-        </div>
+        </footer>
       </div>
     </div>
 </div>
