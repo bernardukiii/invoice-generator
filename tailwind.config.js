0c927e79f29cb51b13aab409450526d50600989a
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-03-05 15:17:58 -0300

    Revert "Chore: adding Flowbite component library"
    This reverts commit cfb1dfa0be805d03f773f8251dccf85487746f4d.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
-  plugins: [require('flowbite/plugin')],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
