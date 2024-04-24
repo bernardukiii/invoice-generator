88397df76ad11aa0e131bb312dddd9225e99ae0e
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-04-24 16:19:21 -0300

    Chore: removing header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,11 +37,6 @@
 </script>
 
 <main class="p-4 m-4">
-    <header>
-        <h1 class="text-gray-200 text-4xl font-semibold" >Invoice generator</h1>
-        <p class="text-gray-200 text-2xl font-normal" >Fill in your details and generate your invoice</p>
-    </header>
-
     <section class="w-full">
         <svelte:component this={isPreview ? InvoicePreview : InvoiceForm } />
     </section>
