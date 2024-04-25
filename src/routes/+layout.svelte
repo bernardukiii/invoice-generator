4534c3b97af47b2d200cfe47af96fcb95527de72
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-04-24 22:36:28 -0300

    Feat: injecting web analytics from vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
     import "../app.css"
+
+    // Import the Analytics package, and the SvelteKit dev variable.
+    import { dev } from '$app/environment'
+    import { inject } from '@vercel/analytics'
+
+    // Inject the Analytics functionality
+    inject({ mode: dev ? 'development' : 'production' })
 </script>
   
 <slot /> <!--Like the Reacts equivalent of children-->
